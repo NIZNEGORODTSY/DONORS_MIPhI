517d024ff3a8d3b0b15f2c5dfdb0661f53e16809
Kirill Churin <kirillchurin07@mail.ru>
2025-07-18 20:35:48 +0300

    работа над api core.py и dbapi.py

--- core.py
+++ core.py
@@ -1,17 +1,39 @@
 import dbapi
 
-class AuthAnswers:
-    Register = 0
-    Success = 1
+def get_uid(tgid: int) -> int:
+    res = dbapi.get_uid(tgid)
+    return res[0]
 
-def check_user_by_phone(phone_number: str) -> AuthAnswers:
+def check_user_by_phone(phone_number: str) -> bool:
     users = dbapi.get_users_gy_phone(phone_number)
-    if (len(users) == 0):
-        return AuthAnswers.Register
-    return AuthAnswers.Success
+    if len(users) == 0:
+        return False
+    return True
 
-def check_admin(phone_number: str) -> bool:
-    is_adm = dbapi.get_admin(phone_number)
-    return is_adm[0] == 1
+def check_admin(tgid: int) -> bool:
+    res = dbapi.get_admin(tgid)
+    return res[0] == 1
 
-check_admin('+73323945103')
+def add_user(phone_number: str, tgid: int):
+    dbapi.add_user(phone_number, tgid)
+
+def add_fio(tgid: int, fio: str):
+    dbapi.add_fio(tgid, fio)
+
+def add_ugroup(tgid: int, ugroup: str):
+    dbapi.add_ugroup(tgid, ugroup)
+
+def add_contacts(tgid: int, contacts: str):
+    dbapi.add_contacts(tgid, contacts)
+
+def add_donation(tgid: int):
+    uid = get_uid(tgid)
+    dbapi.add_donation(uid)
+
+def add_donation_donplace(tgid: int, donplace: int): # 0 - Гавр, 1 - ФМБА
+    uid = get_uid(tgid)
+    dbapi.add_donation_donplace(uid, donplace)
+
+def get_user(tgid: int):
+    res = dbapi.get_user(tgid)
+    return res[0]
--- dbapi.py
+++ dbapi.py
@@ -4,25 +4,6 @@ import config.reader as reader
 reader.read_config()
 
 
-def some_function():
-    def my_query(cursor):
-        query = f"""some query"""
-        cursor.execute(query)
-        return cursor.fetchone()  # Какой-нибудь fetch
-
-    res = execute(my_query)
-    return res
-
-def get_users_gy_phone(phone: str) -> list:
-    def my_query(cursor):
-        query = f"""SELECT * FROM users WHERE PhoneNumber='{phone}'"""
-        cursor.execute(query)
-        return cursor.fetchall()
-    res = execute(my_query)
-    return res
-
-
-
 def execute(dbfun):
     conn = psycopg2.connect(
         #dbname=reader.get_param_value('dbname'),
@@ -38,11 +19,108 @@ def execute(dbfun):
     conn.close()
     return res
 
-
-def get_admin(number):
+# Шаблон
+def some_function():
     def my_query(cursor):
-        query = f"""SELECT isadmin FROM users WHERE PhoneNumber='{number}'"""
+        query = f"""some query"""
+        cursor.execute(query)
+        return cursor.fetchone()
+
+    res = execute(my_query)
+    return res
+
+def get_users_gy_phone(phone: str) -> list:
+    def my_query(cursor):
+        query = f"""SELECT * FROM users WHERE PhoneNumber = '{phone}'"""
+        cursor.execute(query)
+        return cursor.fetchall()
+    res = execute(my_query)
+    return res
+
+def get_uid(tgid: int):
+    def my_query(cursor):
+        query = f"""SELECT id FROM users WHERE tgid = '{tgid}'"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def get_admin(tgid: int):
+    def my_query(cursor):
+        query = f"""SELECT isadmin FROM users WHERE tgid = '{tgid}'"""
         cursor.execute(query)
         return cursor.fetchone()  # Какой-нибудь fetch
+    res = execute(my_query)
+    return res
+
+def add_user(number: str, tgid: int):
+    def my_query(cursor):
+        query = f"""INSERT INTO users (phonenumber, tgid) VALUES ('{number}', '{tgid}')"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_fio(tgid: int, fio: str):
+    def my_query(cursor):
+        query = f"""UPDATE users SET fio = '{fio}' WHERE tgid = '{tgid}'"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_ugroup(tgid: int, ugroup: str):
+    def my_query(cursor):
+        query = f"""UPDATE users SET ugroup = '{ugroup}' WHERE tgid = '{tgid}'"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_contacts(tgid: int, contacts: str):
+    def my_query(cursor):
+        query = f"""UPDATE users SET contacts = '{contacts}' WHERE tgid = '{tgid}'"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_donation(uid: int):
+    def my_query(cursor):
+        query = f"""INSERT INTO donations (uid) VALUES ({uid})"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_donation_donplace(uid: int, donplace: int):
+    def my_query(cursor):
+        query = f"""UPDATE donations SET donplace = {donplace} WHERE uid = {uid}"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def add_donation_donplace(uid: int, dondate: str):
+    def my_query(cursor):
+        query = f"""UPDATE donations SET dondate = '{dondate}' WHERE uid = {uid}"""
+        cursor.execute(query)
+        return cursor.fetchone()
+    res = execute(my_query)
+    return res
+
+def get_user(tgid: int):
+    def my_query(cursor):
+        query = f"""SELECT * FROM users WHERE tgid = '{tgid}'"""
+        cursor.execute(query)
+        return cursor.fetchall()
+    res = execute(my_query)
+    return res
+
+def get_donation_history(uid: int):
+    def my_query(cursor):
+        query = f"""SELECT * FROM donations WHERE uid = {uid} ORDER BY dondate"""
+        cursor.execute(query)
+        return cursor.fetchall()
     res = execute(my_query)
     return res
