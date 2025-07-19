4171d804a819d0e6297dde22679b343f30be265a
GlebNegodov <150227071+GlebNegodov@users.noreply.github.com>
2025-07-19 13:38:45 +0300

    65

--- keybord/user.py
+++ keybord/user.py
@@ -30,7 +30,8 @@ def get_main_menu_keyboard():
             [KeyboardButton(text="📅 Записаться на донацию")],
             [KeyboardButton(text="ℹ️ Информация о донорстве")],
             [KeyboardButton(text="🌤 Рекомендации для доноров на сегодня")],
-            [KeyboardButton(text="❓ Задать вопрос")]
+            [KeyboardButton(text="❓ Задать вопрос")],
+            [KeyboardButton(text="Ответы на ваши вопросы")]
         ],
         resize_keyboard=True
     )
